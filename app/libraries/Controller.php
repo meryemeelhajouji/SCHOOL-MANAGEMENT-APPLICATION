36bb4268b5198f229c8f9c43fa3e7ba35e7e722a
touza <touzaelhassan@gmail.com>
2022-05-08 23:49:12 +0100

    Creating The controller class

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -6,6 +6,19 @@
 class Controller
 {
   // 1 - Create model method That return The current model
+  public function model($model)
+  {
+    require_once "../app/models/$model.php";
+    return new $model();
+  }
 
   // 2 - Create View method That return The current View
+  public function view($view, $data = [])
+  {
+    if (file_exists("../app/views/$view.php")) {
+      require_once "../app/views/$view.php";
+    } else {
+      die('View does not exist');
+    }
+  }
 }
